bf3deb08a58f7ac3943d3d8e8c55bb7aeb411ada
Scripted Adventurer <scripted.adventurer@fake.com>
2020-06-26 17:00:20 -0400

    Added code for board class.

--- board.py
+++ board.py
@@ -1,5 +1,86 @@
+import itertools 
+
 class Board:
-  ''' '''
+  '''Logically represents the board used in gameplay, including methods to make a
+  move and check for a winner.'''
   def __init__(self):
-    # 6 rows, 7 columns
-    pass
+    self.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    ]
+  def move(self, column, player1):
+    # place the piece in the column in the first empty slot from the bottom
+    # return True if successful, False if the column is full
+    if player1:
+      piece = 'X'
+    else:
+      piece = 'O'
+    for row in range(5, -1, -1):
+      if self.board[row][column] == ' ':
+        self.board[row][column] = piece
+        return True
+    return False
+  def _horizontal_winner(self, row, column):
+    # check if there is a horizontal connect 4 to the right (higher column) from here
+    # skip checking columns 4 - 6 as any connect 4 here would be captured earlier
+    if column > 3:
+      return False
+    if (self.board[row][column] != ' ' and 
+      self.board[row][column] == self.board[row][column + 1] and 
+      self.board[row][column] == self.board[row][column + 2] and 
+      self.board[row][column] == self.board[row][column + 3]):
+      return True
+    else:
+      return False  
+  def _vertical_winner(self, row, column):
+    # check if there is a vertical connect 4 up (lower row) from here
+    # skip checking rows 0 - 2 as any connect 4 here would be captured earlier
+    if row < 3:
+      return False
+    if (self.board[row][column] != ' ' and 
+      self.board[row][column] == self.board[row - 1][column] and 
+      self.board[row][column] == self.board[row - 2][column] and 
+      self.board[row][column] == self.board[row - 3][column]):
+      return True
+    else:
+      return False  
+  def _diagonal_up_winner(self, row, column):
+    # check if there is a diagonal connect 4 up and to the right from here
+    # skip rows 0 - 2 and columns 4 - 6 as up-right diagonals are not possible from there 
+    if row < 3 or column > 3:
+      return False 
+    if (self.board[row][column] != ' ' and 
+      self.board[row][column] == self.board[row - 1][column + 1] and 
+      self.board[row][column] == self.board[row - 2][column + 2] and 
+      self.board[row][column] == self.board[row - 3][column + 3]):
+      return True
+    else:
+      return False 
+  def _diagonal_down_winner(self, row, column):
+    # check if there is a diagonal connect 4 down and to the right from here
+    # skip rows 0 - 2 and columns 4 - 6 as down-right diagonals are not possible from there 
+    if row >= 3 or column > 3:
+      return False 
+    if (self.board[row][column] != ' ' and 
+      self.board[row][column] == self.board[row + 1][column + 1] and 
+      self.board[row][column] == self.board[row + 2][column + 2] and 
+      self.board[row][column] == self.board[row + 3][column + 3]):
+      return True
+    else:
+      return False 
+  def check_winner(self):
+    # search bottom (highest row) to top, left (lowest column) to right 
+    rows = range(5, -1, -1)
+    columns = range(7)
+    for row, column in itertools.product(rows, columns):
+      if (self._horizontal_winner(row, column) or self._vertical_winner(row, column) 
+        or self._diagonal_up_winner(row, column) or 
+        self._diagonal_down_winner(row, column)):
+        return True
+    return False     
+  def get_board(self):
+    return self.board  
--- tests.py
+++ tests.py
@@ -5,62 +5,236 @@ import sys
 from board import Board
 from cli import CLI
 
-class TestIO(unittest.TestCase):
+# class TestIO(unittest.TestCase):
 
-  def setUp(self):
-    self.cli = CLI()
+#   def setUp(self):
+#     self.cli = CLI()
 
-  def test_input_difficulty(self):
-    # map each valid input to its expected output
-    valid_data = {}
-    for level in ['Easy', 'Medium', 'Hard']:
-      value = level[0]
-      valid_data[level] = value
-      valid_data[level.upper()] = value
-      valid_data[level.lower()] = value
-      valid_data[level[0]] = value
-      valid_data[level[0].lower()] = value
-    for input_str, return_val in valid_data.items():
-      with unittest.mock.patch('builtins.input', return_value=input_str):
-        self.assertEqual(self.cli.input_difficulty(), return_val)
-    invalid_data = iter(['', 'a', ' ', '\n', 0, 'xyz', '5', '$', '__', 'v'])
-    with unittest.mock.patch('builtins.input', return_value=next(invalid_data)):
-      self.assertRaises(SystemExit, self.cli.input_difficulty)
-  def test_input_move(self):
-    valid_data = ['0', '1', '2', '3', '4', '5', '6']
-    for input_str in valid_data:
-      with unittest.mock.patch('builtins.input', return_value=input_str):
-        self.assertEqual(self.cli.input_move(), int(input_str))
-    invalid_data = iter(['', 'a', ' ', '\n', 'three', 'z', '$', '_', '+', 'wetw4t4'])
-    with unittest.mock.patch('builtins.input', return_value=next(invalid_data)):
-      self.assertRaises(SystemExit, self.cli.input_move)
-  def test_output_board(self):
-    empty_board = [
-      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    ]
-    self.cli.output_board(empty_board)
-    full_board = [
-      ['O', 'O', 'O', 'X', 'O', 'O', 'X'],
-      ['X', 'X', 'X', 'O', 'X', 'X', 'O'],
-      ['O', 'X', 'O', 'O', 'X', 'X', 'X'],
-      ['O', 'O', 'X', 'O', 'X', 'O', 'O'],
-      ['O', 'X', 'O', 'X', 'O', 'X', 'X'],
-      ['X', 'O', 'X', 'X', 'O', 'X', 'O']
-    ]
-    self.cli.output_board(full_board)
+#   def test_input_difficulty(self):
+#     # map each valid input to its expected output
+#     valid_data = {}
+#     for level in ['Easy', 'Medium', 'Hard']:
+#       value = level[0]
+#       valid_data[level] = value
+#       valid_data[level.upper()] = value
+#       valid_data[level.lower()] = value
+#       valid_data[level[0]] = value
+#       valid_data[level[0].lower()] = value
+#     for input_str, return_val in valid_data.items():
+#       with unittest.mock.patch('builtins.input', return_value=input_str):
+#         self.assertEqual(self.cli.input_difficulty(), return_val)
+#     invalid_data = iter(['', 'a', ' ', '\n', 0, 'xyz', '5', '$', '__', 'v'])
+#     with unittest.mock.patch('builtins.input', return_value=next(invalid_data)):
+#       self.assertRaises(SystemExit, self.cli.input_difficulty)
+#   def test_input_move(self):
+#     valid_data = ['0', '1', '2', '3', '4', '5', '6']
+#     for input_str in valid_data:
+#       with unittest.mock.patch('builtins.input', return_value=input_str):
+#         self.assertEqual(self.cli.input_move(), int(input_str))
+#     invalid_data = iter(['', 'a', ' ', '\n', 'three', 'z', '$', '_', '+', 'wetw4t4'])
+#     with unittest.mock.patch('builtins.input', return_value=next(invalid_data)):
+#       self.assertRaises(SystemExit, self.cli.input_move)
+#   def test_output_board(self):
+#     empty_board = [
+#       [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+#       [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+#       [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+#       [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+#       [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+#       [' ', ' ', ' ', ' ', ' ', ' ', ' ']
+#     ]
+#     self.cli.output_board(empty_board)
+#     full_board = [
+#       ['O', 'O', 'O', 'X', 'O', 'O', 'X'],
+#       ['X', 'X', 'X', 'O', 'X', 'X', 'O'],
+#       ['O', 'X', 'O', 'O', 'X', 'X', 'X'],
+#       ['O', 'O', 'X', 'O', 'X', 'O', 'O'],
+#       ['O', 'X', 'O', 'X', 'O', 'X', 'X'],
+#       ['X', 'O', 'X', 'X', 'O', 'X', 'O']
+#     ]
+#     self.cli.output_board(full_board)
            
 class TestBoard(unittest.TestCase):
 
   def setUp(self):
-    pass
+    self.board = Board()
 
-  def test_1(self):
-    pass  
+  def test_move(self):
+    self.assertEqual(self.board.move(column=3, player1=True), True)
+    self.assertEqual(self.board.move(column=4, player1=False), True)
+    self.assertEqual(self.board.move(column=4, player1=True), True)
+    self.assertEqual(self.board.move(column=3, player1=False), True)
+    self.assertEqual(self.board.move(column=2, player1=True), True)
+    self.assertEqual(self.board.move(column=1, player1=False), True)
+    self.assertEqual(self.board.move(column=2, player1=True), True)
+    self.assertEqual(self.board.move(column=4, player1=False), True)
+    self.assertEqual(self.board.move(column=2, player1=True), True)
+    self.assertEqual(self.board.move(column=2, player1=False), True)
+    self.assertEqual(self.board.get_board(), [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', 'O', ' ', ' ', ' ', ' '],
+      [' ', ' ', 'X', ' ', 'O', ' ', ' '],
+      [' ', ' ', 'X', 'O', 'X', ' ', ' '],
+      [' ', 'O', 'X', 'X', 'O', ' ', ' ']
+    ])
+    self.assertEqual(self.board.move(column=2, player1=True), True)
+    self.assertEqual(self.board.move(column=2, player1=False), True) 
+    # full column test
+    self.assertEqual(self.board.move(column=2, player1=True), False)
+    self.assertEqual(self.board.move(column=2, player1=False), False)
+    self.assertEqual(self.board.get_board(), [
+      [' ', ' ', 'O', ' ', ' ', ' ', ' '],
+      [' ', ' ', 'X', ' ', ' ', ' ', ' '],
+      [' ', ' ', 'O', ' ', ' ', ' ', ' '],
+      [' ', ' ', 'X', ' ', 'O', ' ', ' '],
+      [' ', ' ', 'X', 'O', 'X', ' ', ' '],
+      [' ', 'O', 'X', 'X', 'O', ' ', ' ']
+    ])
+
+  def test_check_winner(self):
+    # pattern these out somehow 
+
+    # no winners
+    self.board.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '] ]
+    self.assertEqual(self.board.check_winner(), False)
+    self.board.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', 'X', 'O', ' ', ' ', ' ', ' '],
+      [' ', 'O', 'O', 'X', 'O', ' ', ' '],
+      [' ', 'X', 'X', 'O', 'X', 'O', ' '],
+      [' ', 'O', 'X', 'X', 'O', 'X', ' '] ]
+    self.assertEqual(self.board.check_winner(), False)
+    self.board.board = [
+      ['X', 'O', 'X', 'X', 'O', 'X', 'O'],
+      ['X', 'X', 'O', 'O', 'O', 'X', 'X'],
+      ['X', 'O', 'X', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'X', 'X', 'X'],
+      ['X', 'X', 'O', 'X', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'X', 'O', 'X', 'X'] ]
+    self.assertEqual(self.board.check_winner(), False)
+    # vertical winners
+    self.board.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      ['O', ' ', ' ', ' ', ' ', ' ', ' '],
+      ['O', ' ', ' ', ' ', ' ', ' ', ' '],
+      ['O', ' ', ' ', 'X', ' ', ' ', ' '],
+      ['O', ' ', 'X', 'X', 'X', ' ', ' '] ]
+    self.assertEqual(self.board.check_winner(), True)
+    self.board.board = [
+      ['X', ' ', ' ', ' ', ' ', ' ', ' '],
+      ['X', ' ', ' ', ' ', ' ', ' ', ' '],
+      ['X', ' ', ' ', ' ', ' ', ' ', ' '],
+      ['X', ' ', ' ', ' ', ' ', 'O', ' '],
+      ['O', ' ', ' ', 'X', 'X', 'O', ' '],
+      ['O', ' ', 'X', 'O', 'O', 'X', 'O'] ]
+    self.assertEqual(self.board.check_winner(), True)
+    self.board.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', 'X', ' ', ' ', ' '],
+      [' ', ' ', ' ', 'X', 'O', ' ', ' '],
+      [' ', ' ', 'O', 'X', 'X', ' ', ' '],
+      [' ', ' ', 'O', 'X', 'O', ' ', ' '] ]
+    self.assertEqual(self.board.check_winner(), True)
+    self.board.board = [
+      [' ', ' ', ' ', ' ', 'O', ' ', ' '],
+      [' ', ' ', ' ', ' ', 'O', ' ', ' '],
+      [' ', ' ', ' ', ' ', 'O', ' ', ' '],
+      [' ', ' ', ' ', 'X', 'O', ' ', ' '],
+      [' ', ' ', 'X', 'X', 'X', ' ', 'X'],
+      [' ', ' ', 'O', 'O', 'X', 'O', 'X'] ]
+    self.assertEqual(self.board.check_winner(), True)
+    self.board.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', 'X'],
+      [' ', ' ', ' ', ' ', ' ', ' ', 'X'],
+      [' ', ' ', 'O', ' ', ' ', ' ', 'X'],
+      [' ', ' ', 'O', 'O', ' ', 'O', 'X'] ]
+    self.assertEqual(self.board.check_winner(), True)
+    self.board.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', 'O'],
+      [' ', ' ', ' ', ' ', ' ', ' ', 'O'],
+      [' ', ' ', ' ', ' ', ' ', ' ', 'O'],
+      [' ', ' ', ' ', ' ', ' ', ' ', 'O'],
+      [' ', ' ', 'X', ' ', ' ', ' ', 'X'],
+      [' ', 'O', 'X', 'X', 'O', 'X', 'X'] ]
+    self.assertEqual(self.board.check_winner(), True)
+    # horizontal winner
+    self.board.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', 'O', 'O', ' ', ' '],
+      [' ', 'X', 'X', 'X', 'X', 'O', ' '] ]
+    self.assertEqual(self.board.check_winner(), True)
+    self.board.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', 'X', 'X', ' ', ' '],
+      ['O', 'O', 'O', 'O', 'X', 'X', ' '] ]
+    self.assertEqual(self.board.check_winner(), True)
+    self.board.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', 'X', 'X', ' ', ' '],
+      [' ', 'X', 'X', 'O', 'O', 'O', 'O'] ]
+    self.assertEqual(self.board.check_winner(), True)
+    self.board.board = [
+      ['X', 'X', 'X', 'X', 'O', 'X', 'O'],
+      ['X', 'X', 'O', 'O', 'O', 'X', 'X'],
+      ['X', 'O', 'O', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'X', 'X', 'X'],
+      ['X', 'X', 'O', 'X', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'X', 'O', 'X', 'X'] ]
+    self.assertEqual(self.board.check_winner(), True)
+    self.board.board = [
+      ['X', 'X', 'X', 'O', 'O', 'O', 'O'],
+      ['X', 'X', 'O', 'X', 'O', 'X', 'X'],
+      ['X', 'O', 'X', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'X', 'X', 'X'],
+      ['X', 'X', 'O', 'X', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'X', 'O', 'X', 'X'] ]
+    self.assertEqual(self.board.check_winner(), True)
+    self.board.board = [
+      ['X', 'O', 'X', 'X', 'X', 'X', 'O'],
+      ['X', 'X', 'O', 'X', 'O', 'X', 'X'],
+      ['X', 'O', 'X', 'O', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'O', 'X', 'X', 'X'],
+      ['X', 'X', 'O', 'X', 'X', 'O', 'O'],
+      ['O', 'O', 'X', 'X', 'O', 'X', 'X'] ]
+    self.assertEqual(self.board.check_winner(), True)
+    # diagonal winners
+    self.board.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', 'O', ' ', ' ', ' ', ' ', ' '],
+      [' ', 'X', 'O', ' ', ' ', ' ', ' '],
+      [' ', 'X', 'X', 'O', 'X', ' ', ' '],
+      [' ', 'O', 'X', 'X', 'O', 'O', ' '] ]
+    self.assertEqual(self.board.check_winner(), True)
+    self.board.board = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', 'O'],
+      [' ', ' ', ' ', ' ', ' ', 'O', 'X'],
+      [' ', ' ', ' ', 'X', 'O', 'O', 'O'],
+      [' ', ' ', ' ', 'O', 'X', 'X', 'X'],
+      [' ', ' ', ' ', 'X', 'X', 'O', 'X'] ]
+    self.assertEqual(self.board.check_winner(), True)
 
 if __name__ == '__main__':
   unittest.main()
